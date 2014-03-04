df98ebf2b927ef24f24a7695183413f92e2c602a
ammar falmban <devAspirant@gmail.com>
2014-03-04 22:33:27 +0900

    Initial Commit

---
+++ CoreMotionWithUIImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreMotionWithUIImage
+//
+//  Created by ammar falmban on 3/4/14.
+//  Copyright (c) 2014 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreMotionWithUIImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreMotionWithUIImage
+//
+//  Created by ammar falmban on 3/4/14.
+//  Copyright (c) 2014 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
